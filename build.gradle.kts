a41e5cc53820ae85cd3501c29c723bb75e51768d
985892345 <guo985892345@foxmail.com>
2024-01-14 17:34:11 +0800

    :bookmark: release 1.3.2-alpha02-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.jetbrains.compose) apply false
-  id("io.github.985892345.KtProvider") version "1.3.2-alpha01-SNAPSHOT" apply false // 测试时记得改这里的版本号
+  id("io.github.985892345.KtProvider") version "1.3.2-alpha02-SNAPSHOT" apply false // 测试时记得改这里的版本号
 }
 
 allprojects {
